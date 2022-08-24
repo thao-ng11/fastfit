c5f110782837832bd2421318623cebf76f0a25fb
Thao <thao.ng11@gmail.com>
2022-08-24 18:27:21 -0400

    work progress aug 24 on journal backend

---
+++ backend/journals/api/routers/journal.py
@@ -0,0 +1,11 @@
+4.66 KiB
+from fastapi import APIRouter, Response, status, Depends, HTTPException
+from pydantic import BaseModel
+from typing import List
+from fastapi.security import OAuth2PasswordBearer
+import os
+from psycopg_pool import ConnectionPool
+
+conninfo = os.environ["DATABASE_URL"]
+pool = ConnectionPool(conninfo=conninfo)
+
