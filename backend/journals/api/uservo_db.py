02ff5dc8209a45db335c68f88515a817ff1ce47e
Thao <thao.ng11@gmail.com>
2022-08-30 14:50:59 -0400

    remove poller from journal

--- backend/journals/api/uservo_db.py
+++
@@ -1,9 +0,0 @@
-import os
-from psycopg_pool import ConnectionPool
-
-conninfo = os.environ["DATABASE_URL"]
-pool = ConnectionPool(conninfo=conninfo)
-
-class UserVOQueries: 
-    def get_uservo(self):
-        pass
