5c7127393c3f3974dca0235451e07f68519c7428
Ryan <rcramer5@verizon.net>
2022-08-31 09:04:57 -0500

    added account remote host to docker compose

--- frontend/fast-fit-frontend/src/Dashboard.js
+++ frontend/fast-fit-frontend/src/Dashboard.js
@@ -1,3 +1,19 @@
 import React from 'react';
 
 
+function Map(){
+   return(
+    <iframe
+    width="600"
+    height="450"
+    style="border:0"
+    loading="lazy"
+    allowfullscreen
+    referrerpolicy="no-referrer-when-downgrade"
+    src="https://www.google.com/maps/embed/v1/place?key=API_KEY&q=Space+Needle,Seattle+WA">
+</iframe>
+   );
+
+}
+
+export default 
