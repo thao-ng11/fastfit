68268c27180cf4861d1ee1223fcda58478981573
Thao <thao.ng11@gmail.com>
2022-09-01 18:05:02 -0400

    JournalForm outline

--- frontend/fast-fit-frontend/src/App.js
+++ frontend/fast-fit-frontend/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Nav from './Nav';
-import Dashboard from './Dashboard';
-import SignUp from './Auth/Signup';
-import WorkoutPlanForm from './Workouts/WorkoutPlanForm'
-import UserMeals from './Recipes/userMeals';
-import HealthDataForm from './health_data/HealthMain';
-
+import Nav from "./Nav";
+import Dashboard from "./Dashboard";
+import SignUp from "./Auth/Signup";
+import WorkoutPlanForm from "./Workouts/WorkoutPlanForm";
+import UserMeals from "./Recipes/userMeals";
+import HealthDataForm from "./health_data/HealthMain";
+import JournalForm from "./journals/JournalForm";
 
 function App() {
   return (
@@ -21,6 +21,7 @@ function App() {
           <Route path="/workout" element={<WorkoutPlanForm />} />
           <Route path="/meals" element={<UserMeals />} />
           <Route path="/health" element={<HealthDataForm />} />
+          <Route path="/journal" element={<JournalForm />} />
         </Routes>
       </div>
     </BrowserRouter>
