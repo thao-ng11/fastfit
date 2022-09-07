65a56cbe0ab6be2db1e7e39e2b08a7b15cee1d5e
Drew <drewsaenz11@gmail.com>
2022-09-07 18:06:39 -0500

    installed react-chart-js-2 in the run.sh, started chart js file

--- frontend/fast-fit-frontend/run.sh
+++ frontend/fast-fit-frontend/run.sh
@@ -2,4 +2,5 @@ npm install
 npm install -D tailwindcss
 npm install -D postcss 
 npm install -D autoprefixer
+npm install --save chart.js react-chartjs-2
 npm start
--- frontend/fast-fit-frontend/src/health_data/Chart.js
+++ frontend/fast-fit-frontend/src/health_data/Chart.js
@@ -1,5 +1,15 @@
+import React from "react"
+import { Line } from "react-chartjs-2"
+import format from "date-fns/format"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
 
-<div>
-  <canvas id="myChart"></canvas>
-</div>
