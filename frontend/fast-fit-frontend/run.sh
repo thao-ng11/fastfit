33cadabb4675fe4fdbce35fb47ad107a9c402134
Drew <drewsaenz11@gmail.com>
2022-09-08 16:01:51 -0500

    rendered a chart

--- frontend/fast-fit-frontend/run.sh
+++ frontend/fast-fit-frontend/run.sh
@@ -2,5 +2,7 @@ npm install
 npm install -D tailwindcss
 npm install -D postcss 
 npm install -D autoprefixer
+npm i chart.js
+npm i --legacy-peer-deps react-chartjs-2
 npm install --save chart.js react-chartjs-2
 npm start
--- frontend/fast-fit-frontend/src/health_data/HealthMain.js
+++ frontend/fast-fit-frontend/src/health_data/HealthMain.js
@@ -1,46 +1,70 @@
-import React, { Component } from 'react'
-import Chart from 'chart.js'
+import React from "react"
+// import { Line } from "react-chartjs-2";
+import format from "date-fns/format"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
+  ChartOptions
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
 
-function HealthDataForm(  ) {
-    const labels = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-    ];
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    TimeScale,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+export default function HealthDataForm() {
+  const weightHistory = [
+    {current_weight: 180, entry_date: "2022-08-30"},
+    {current_weight: 175, entry_date: "2022-08-31"},
+    {current_weight: 170, entry_date: "2022-09-21"},
+    {current_weight: 180, entry_date: "2022-09-22"}
+  ]
   
-    const data = {
-      labels: labels,
-      datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-      }]
-    };
+    let labels = [];
+    let weights = [];
   
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-  
-    const myChart = new Chart(
-      document.getElementById('myChart'),
-      config
-    );
+    weightHistory &&
+      weightHistory.map((test) => {
+        const date = format(new Date(test.entry_date), "MM/dd/yy");
+        labels.push(date);
+        weights.push("Hello");
+      });
     
-    return (
-        <>
-        <h1> Health data </h1>
-        <div>
-          <canvas id="myChart"></canvas>
-        </div>
-        </>
-    )
-  }
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: "Weight",
+          data: weights,
+          backgroundColor: ["rbga(129, 178, 154, 0.6)"],
+          borderWidth: 2,
+          lineTension: 0,
+          borderDashOffset: 0.0,
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+        },
+      ],
+    };
+    return <Line data={data} height={90} type="line" />;
+}  
 
-
-export default HealthDataForm;
