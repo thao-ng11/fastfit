56576fec6e46bbd044cd411da12499a6e1c56a3e
Thao <thao.ng11@gmail.com>
2022-09-08 16:43:55 -0400

    changing the App.js to avoid conflict when merging

--- frontend/fast-fit-frontend/src/App.js
+++ frontend/fast-fit-frontend/src/App.js
@@ -1,34 +1,63 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useToken } from "./Authentication";
 import Nav from "./Nav";
 import Dashboard from "./Dashboard";
 import SignUp from "./Auth/Signup";
 import WorkoutPlanForm from "./Workouts/WorkoutPlanForm";
 import UserMeals from "./Recipes/userMeals";
+import RecipeSearch from "./Recipes/recipeSearch";
 import HealthDataForm from "./health_data/HealthMain";
 import JournalForm from "./journals/journalForm";
-import JournalMain from "./journals/journalMain";
-import JournalsList from "./journals/journalList";
+import Login from "./Auth/Login";
+import WorkoutPlanList from "./Workouts/WorkoutPlanList";
+import { AuthProvider } from "./Authentication";
 
 function App() {
+  const [token, login, logout, signup, update] = useToken();
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
   return (
-    <BrowserRouter>
-      <Nav />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/workout" element={<WorkoutPlanForm />} />
-          <Route path="/meals" element={<UserMeals />} />
-          <Route path="/health" element={<HealthDataForm />} />
-          <Route path="/journals/form" element={<JournalForm />} />
-          <Route path="/journals" element={<JournalMain />} />
-          <Route path="/journals/details" element={<JournalsList />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <AuthProvider>
+        <Nav />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard lat={lat} lng={lng} />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login login={login} />} />
+            <Route path="/workout" element={<WorkoutPlanForm />} />
+            <Route path="/meals">
+              <Route path="user" element={<UserMeals />} />
+              <Route path="search" element={<RecipeSearch />} />
+            </Route>
+            {/* <Route path="/health" element={<HealthDataForm />} /> */}
+            <Route path="/journal" element={<JournalForm />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </>
   );
 }
 
