7f99e4c0fefdc877ad7abf6c3a6e0e62aba882d0
Thao <thao.ng11@gmail.com>
2022-09-09 21:18:08 -0400

    adding tailwind to thee main journal page

--- frontend/fast-fit-frontend/src/journals/journalEmoji.js
+++ frontend/fast-fit-frontend/src/journals/journalEmoji.js
@@ -26,13 +26,13 @@ function JournalEmoji(props) {
         return [faFaceSmile, "good"];
         break;
       case 3:
-        return [faFaceMeh, "meh"];
+        return [faFaceMeh, " meh"];
         break;
       case 4:
-        return [faFaceFrown, "sad"];
+        return [faFaceFrown, " sad"];
         break;
       case 5:
-        return [faFaceTired, "distraught"];
+        return [faFaceTired, "somber"];
         break;
 
       default:
@@ -45,15 +45,21 @@ function JournalEmoji(props) {
 
   function renderStyle() {
     if (props.emojiValue === props.activeEmoji) {
-      return { color: "green" };
+      return {
+        color: "green",
+        height: "45px",
+        width: "45px",
+      };
     } else {
       return {
         color: "gray",
+        height: "45px",
+        width: "45px",
       };
     }
   }
   return (
-    <div className="emoji" onClick={handleActive}>
+    <div className="flex flex-col" onClick={handleActive}>
       <FontAwesomeIcon style={renderStyle()} icon={emoji[0]} />
       <span>{emoji[1]}</span>
     </div>
