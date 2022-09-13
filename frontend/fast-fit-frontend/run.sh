50811ae52528437537196a293a610d63a483d6fd
Ryan <rcramer5@verizon.net>
2022-09-13 09:10:39 -0500

    merge resolution

--- frontend/fast-fit-frontend/run.sh
+++ frontend/fast-fit-frontend/run.sh
@@ -5,8 +5,8 @@ npm install -D autoprefixer
 npm i chart.js
 npm i --legacy-peer-deps react-chartjs-2
 npm install --save chart.js react-chartjs-2
-npm install -D @react-google-maps/api   
-npm install -D @reach/combobox 
-npm install -D use-places-autocomplete
-npm install -D date-fns        
+npm install -D --legacy-peer-deps @react-google-maps/api   
+npm install -D --legacy-peer-deps @reach/combobox 
+npm install -D --legacy-peer-deps use-places-autocomplete
+npm install -D --legacy-peer-deps date-fns        
 npm start
