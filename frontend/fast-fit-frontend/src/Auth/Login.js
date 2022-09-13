8c7ed7b5adfc3c9c29dea9c04386b05f8ef9ccc0
Ryan <rcramer5@verizon.net>
2022-09-13 13:29:13 -0500

    changed the landing page to be the login and the dashboard to /dashboard

--- frontend/fast-fit-frontend/src/Auth/Login.js
+++ frontend/fast-fit-frontend/src/Auth/Login.js
@@ -20,7 +20,7 @@ export default function Login()
             username: "",
             password: '',
         })
-        navigate('/')
+        navigate('/dashboard')
     }
 
     return(
--- frontend/fast-fit-frontend/src/Dashboard.js
+++ frontend/fast-fit-frontend/src/Dashboard.js
@@ -8,10 +8,6 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      recipe_router: false,
-      workout_router: false,
-      health_router: false,
-      journal_router: false,
       bmi_calculated: 0,
       calc_height: "",
       calc_weight: "",
@@ -42,18 +38,6 @@ class Dashboard extends React.Component {
   }
   
   render() {
-    if (this.state.health_router === true) {
-      return <Navigate to="/health" userInput={this.state.userInput} />;
-    }
-    if (this.state.journal_router === true) {
-      return <Navigate to="/journal" userInput={this.state.userInput} />;
-    }
-    if (this.state.workout_router === true) {
-      return <Navigate to="/workout" userInput={this.state.userInput} />;
-    }
-    if (this.state.recipe_router === true) {
-      return <Navigate to="/recipe" userInput={this.state.userInput} />;
-    }
     let bmiLong =
       (this.state.calc_weight /
         (this.state.calc_height * this.state.calc_height)) *
@@ -112,9 +96,11 @@ class Dashboard extends React.Component {
         </div>
         <div className="max-w-7xl px-5 py-2.5 bg-[#073b4c] flex space-x-5 w-full h-full items-center justify-center mx-auto">
           <div className="flex flex-col space-y-5 w-full h-full">
-            <div className="flex py-2.5 h-1/2 w-full bg-[#c7e8f3] rounded-md justify-center">
+          <a href="../workout/" className="flex py-2.5 h-1/2 w-full bg-[#c7e8f3] rounded-md justify-center" >
+            <div className="flex py-2.5 h-1/2 w-full bg-[#c7e8f3] rounded-md justify-center" >
               <h1 className="text-3xl font-semibold text-[#8e4162]"> Workouts </h1>
             </div>
+          </a>
             <div className="flex py-2.5 h-1/2 w-full bg-[#c7e8f3] rounded-md justify-center">
               <h1 className="text-3xl font-semibold text-[#8e4162]">Journal</h1>
             </div>
