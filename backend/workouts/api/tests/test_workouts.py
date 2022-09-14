9b4afbacb6cddc2278e49359b98adee153100cd4
Nate Endicott <Nateendicott28@gmail.com>
2022-09-14 10:57:50 -0400

    Started unit tests on the backend

---
+++ backend/workouts/api/tests/test_workouts.py
@@ -0,0 +1,75 @@
+from fastapi.testclient import TestClient
+from main import app
+from workouts_db import  StrengthWorkoutQueries
+
+class EmptyStrengthWorkoutQueries:
+    def get_all_strength_workouts(self):
+        print("can you see this?")
+        return None
+
+class NormalStrengthQueries:
+    def get_all_strength_workouts(self):
+        return [
+           {
+                "id": 15,
+                "username": "test1",
+                "category": "strength",
+                "muscle_group": "Hamstrings",
+                "workout": "Barbell Deadlift",
+                "workout_date": "2022-09-13",
+                "sets": 4,
+                "repetitions": 4,
+                "weight": 315
+            },
+            {
+                "id": 16,
+                "username": "test1",
+                "category": "strength",
+                "muscle_group": "Chest",
+                "workout": "Pushups",
+                "workout_date": "2022-09-13",
+                "sets": 3,
+                "repetitions": 10,
+                "weight": 150
+            },
+        ]
+
+client = TestClient(app)
+
+def test_get_all_strength_workout_data_returns_404():
+    # ARRANGE
+    # Use  our fake database
+    app.dependency_overrides[StrengthWorkoutQueries] = EmptyStrengthWorkoutQueries
+
+    # ACT
+    # Make the request
+    response = client.get("/api/strength_workout/")
+
+    # ASSERT
+    # Assert that we got a 404
+    assert response.status_code == 404
+
+    # CLEAN UP
+    # Clear out  the dependencies
+    app.dependency_overrides = {}
+
+def test_get_all_strength_workout_data_returns_200():
+    # ARRANGE
+    # Use our fake database
+    app.dependency_overrides[StrengthWorkoutQueries] = NormalStrengthQueries
+
+    # ACT
+    # Make the request
+    response = client.get("/apai/strength_workout/")
+    d = response.json()
+    print(d)
+
+    # ASSERT
+    # Assert that we got a 404
+    assert response.status_code == 200
+    assert d[0]
+    assert d[1]
+
+    # CLEAN UP
+    # Clear out the dependencies
+    app.dependency_overrides = {}
