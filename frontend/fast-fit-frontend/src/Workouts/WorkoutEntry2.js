bd177bf7f421cb583f0174b0191df861ca360dea
Nate Endicott <Nateendicott28@gmail.com>
2022-09-14 15:02:38 -0400

    Finished Workouts Widget

---
+++ frontend/fast-fit-frontend/src/Workouts/WorkoutEntry2.js
@@ -0,0 +1,31 @@
+import React, { useState,} from 'react';
+
+
+function WorkoutEntry({workout, cancelWorkout}) {
+function Delete(){
+    if (workout.muscle_group === 'Abdominals' || workout.workout === 'Jumping rope'){
+       cancelWorkout(workout.id,'cardio_workout')
+}else{
+    cancelWorkout(workout.id, 'strength_workout')
+}
+}
+return (
+    <div  className=' flex item-center justify-center'>
+        
+
+            <div className='flex item-center justify-center pt-1'>
+            <h4 className='font-semibold text-lg '>{workout.workout}</h4>  
+            <h5 className='pl-2 pt-1'>{workout.sets} sets of {workout.repetitions} reps {workout.weight} pounds</h5>
+
+            </div>
+
+        {/* <div className='flex item-center justify-center'>
+            {/* <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">View workout</button> */}
+            {/* <button onClick={Delete} class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+        </div> */}
+
+    </div>
+)
+}
+
+export default WorkoutEntry;
---
+++ frontend/fast-fit-frontend/src/Workouts/WorkoutWidget.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import WorkoutEntry2 from './WorkoutEntry2'
+import { useToken } from '../Authentication'
+import { useNavigate } from 'react-router-dom';
+
+function WorkoutPlanList() {
+    const [token] = useToken()
+    console.log(token)
+    let dates = []
+    const [workouts,setWorkouts] = useState([])
+    const navigate = useNavigate()
+    const [dateFilter,setDateFilter] = useState('')
+    const fetchWorkouts = async () => {
+        const strengthWorkoutstUrl = 'http://localhost:8020/api/strength_workout/user'
+        const cardioWorkoutsUrl ='http://localhost:8020/api/cardio_workout/user'
+        const response = await fetch(strengthWorkoutstUrl, {
+            credentials: "include",
+            headers: {
+                Authorization: `Bearer ${token}` 
+            },
+          })
+        const response2 = await fetch(cardioWorkoutsUrl, {
+            credentials: "include",
+            headers: {
+                Authorization: `Bearer ${token}` 
+            },
+          })
+        const data = await response.json();
+        const data2 = await response2.json();
+        console.log(data)
+        const allData = data.concat(data2)
+        setWorkouts(allData)
+    }
+    useEffect(() => {
+        fetchWorkouts()
+    }, [token]);
+    
+    const cancelWorkout = async (id,type) => {
+        const cancelUrl = `http://localhost:8020/api/${type}/${id}/`
+        const fetchConfig = {
+            method: "delete"
+        }
+        const cancelResponse = await fetch(cancelUrl, fetchConfig)
+        if (cancelResponse.ok){
+            let updatedworkouts = [];
+            updatedworkouts = workouts.filter(workout => workout.id !== id)
+            setWorkouts(updatedworkouts);
+        }
+    }
+    function filterDates(e){
+        setDateFilter(e.target.value);
+    }
+    function handleNavigate(){
+        navigate('/workout')
+    }
+    return (
+        <div className='flex justify-between items-center overflow-y-auto  max-h-[100px]'>
+            <div className='w-full bg-[#c7e8f3] rounded-lg'>
+            <table className="table-auto object-cover table-borderless">
+                <thead>
+                </thead>
+                <tbody >
+                    {workouts.map(workout => {
+                        if (workout.workout_date.includes(dateFilter)){
+                            return (
+                                <WorkoutEntry2 className='hover:bg-gray-300' workout={workout}
+                               />
+                            );
+                        }
+                    })}
+                </tbody>
+            </table>
+            </div>
+            </div>
+          
+        );
+    }
+
+export default WorkoutPlanList;
